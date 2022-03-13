b95c14fa3bf7d0ad4775601518ce53b3ab6d282e
Ryo Akita <ryo0221outlook@gmail.com>
2022-03-13 20:21:24 +0900

    legend added

---
+++ geo/app.js
@@ -0,0 +1,33 @@
+import "leaflet.animatedmarker/src/AnimatedMarker";
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox/streets-v11',
+		tileSize: 512,
+		zoomOffset: -1
+	}).addTo(map);
+
+	var marker = L.marker([51.5, -0.09]).addTo(map);
+
+	var circle = L.circle([51.508, -0.11], {
+		color: 'red',
+		fillColor: '#f03',
+		fillOpacity: 0.5,
+		radius: 500
+	}).addTo(map);
+
+	var line = L.polyline([[40.68510, -73.94136],[40.68576, -73.94149],[40.68649, -73.94165]]),
+    animatedMarker = L.animatedMarker(line.getLatLngs());
+
+	
+
+fx.run(el, [500, 200], 0.5);
+
+	var polygon = L.polygon([
+		[51.509, -0.08],
+		[51.503, -0.06],
+		[51.51, -0.047]
+	]).addTo(map);
--- new_project/b.js
+++ new_project/b.js
@@ -1,2 +1,16 @@
-const a=2;
-const b=3;
+import * as Three from "three";
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+const scene = new Three.Scene();
+const loader = new GLTFLoader();
+
+loader.load( 'path/to/model.glb', function ( gltf ) {
+
+	scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+print('test')
